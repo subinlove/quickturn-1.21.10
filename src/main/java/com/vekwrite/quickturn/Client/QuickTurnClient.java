7b5e6d5fcf2cb90706601bfe8d82fc6d9195a85f
subinlove <subinyeongu@gmail.com>
2026-06-06 12:18:41 +0900

    Update QuickTurnClient.java

--- src/main/java/com/vekwrite/quickturn/Client/QuickTurnClient.java
+++ src/main/java/com/vekwrite/quickturn/Client/QuickTurnClient.java
@@ -1,45 +1,52 @@
 package com.vekwrite.quickturn.Client;
 
 import com.vekwrite.quickturn.QuickTurn;
-import com.vekwrite.quickturn.TurnConfig;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import org.lwjgl.glfw.GLFW;
 
 public class QuickTurnClient implements ClientModInitializer {
 
-    @Override
-    public void onInitializeClient() {
-        try {
-            QuickTurn.config.load();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+```
+private static KeyBinding quickTurnKeyBinding;
 
-        KeyBinding quickTurnKeyBinding;
-        quickTurnKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-           "key.vekquickturn.quickturn",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
-                "category.vekquickturn.keycat"
-        ));
+@Override
+public void onInitializeClient() {
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (quickTurnKeyBinding.wasPressed()) {
-                MinecraftClient.getInstance().player.setYaw(MinecraftClient.getInstance().player.getYaw() + QuickTurn.config.turn_amount);
+    quickTurnKeyBinding =
+            KeyBindingHelper.registerKeyBinding(
+                    new KeyBinding(
+                            "key.vekquickturn.quickturn",
+                            InputUtil.Type.KEYSYM,
+                            GLFW.GLFW_KEY_R,
+                            "category.vekquickturn.keycat"
+                    )
+            );
+
+    ClientTickEvents.END_CLIENT_TICK.register(client -> {
+
+        while (quickTurnKeyBinding.wasPressed()) {
+
+            MinecraftClient mc = MinecraftClient.getInstance();
+
+            if (mc.player == null) {
+                continue;
             }
-        });
 
-    }
+            mc.player.setYaw(
+                    mc.player.getYaw()
+                            + QuickTurn.config.turn_amount
+            );
+        }
+    });
+}
+```
 
 }
